00a23ef6176847f3f3ab6a872c53764032ec3829
osadolortech <brightosadolor8@gmail.com>
2022-06-15 01:26:11 +0100

    procfile setup

--- api/settings.py
+++ api/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from email.policy import default
 from pathlib import Path
-import os
 import django_heroku
 import dj_database_url
 
@@ -25,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRET_KEY')
-
+SECRET_KEY = 'django-insecure-#j!s1ihixlzm*p%ho!q##vw*0tc9rpny--5+qd56lh*y-rkqbm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
